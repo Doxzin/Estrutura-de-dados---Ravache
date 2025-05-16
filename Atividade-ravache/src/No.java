0c3c869208bd3847b10f5c7423c5f63aad701f50
João Gabriel de Oliveira <joaooliveira0529@gmail.com>
2025-05-15 21:57:26 -0300

    Add files via upload

---
+++ Atividade-ravache/src/No.java
@@ -0,0 +1,30 @@
+public class No{
+    private String valor;
+    private No esquerdo, direito;
+
+    public No(String valor){
+        this.valor = valor;
+        this.esquerdo = null;
+        this.direito = null;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public No getEsquerdo() {
+        return esquerdo;
+    }
+
+    public void setEsquerdo(No esquerdo) {
+        this.esquerdo = esquerdo;
+    }
+
+    public No getDireito() {
+        return direito;
+    }
+
+    public void setDireito(No direito) {
+        this.direito = direito;
+    }
+}
