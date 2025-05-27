223f52e1e50d9b7a57282e61f90770d162662c12
Joao gabriel <joaooliveira0529@gmail.com>
2025-05-27 19:08:04 -0300

    preOrdemNR

--- Atividade-ravache/src/Arvore.java
+++ Atividade-ravache/src/Arvore.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Arvore {
     private No raiz = null;
 
@@ -16,6 +18,7 @@ public class Arvore {
     public int contarNo() {
         return contagem(raiz);
     }
+
     public int contagem(No no) {
         if (no == null) {
             return 0;
@@ -26,24 +29,21 @@ public class Arvore {
             return 1 + esquerda + direita;
         }
     }
-    public void preOrdem (No no) {
-        if (no != null){
-            System.out.println(no.getValor());
-            preOrdem(no.getEsquerdo());
-            preOrdem(no.getDireito());
+    public void preOrdemIte(No raiz){
+        if (raiz == null) return;
+            Stack<No> pilha= new Stack<>();
+            pilha.push(raiz);
+
+            while(!pilha.isEmpty()) {
+            No atual = pilha.pop();
+            System.out.println(atual.getValor());
+            if (atual.getDireito() != null) {
+                pilha.push(atual.getDireito());
+            }
+            if (atual.getEsquerdo() != null) {
+                pilha.push(atual.getEsquerdo());
+            }
         }
-    }
-    public void emOrdem (No no){
-        if (no != null);
-            emOrdem(no.getEsquerdo());
-            System.out.println(no.getValor());
-            emOrdem(no.getDireito());
-    }
-    public void posOrdem (No no) {
-        if (no != null) {
-            posOrdem(no.getEsquerdo());
-            posOrdem(no.getDireito());
-            System.out.println((no.getValor()));
-        }
+
     }
 }
--- Atividade-ravache/src/Main.java
+++ Atividade-ravache/src/Main.java
@@ -6,15 +6,11 @@ public class Main {
         arvore.getRaiz().setEsquerdo(new No("B"));
         arvore.getRaiz().setDireito(new No("C"));
         arvore.getRaiz().getEsquerdo().setEsquerdo(new No("D"));
-        arvore.getRaiz().getEsquerdo().setDireito(new No("D"));
+        arvore.getRaiz().getEsquerdo().setDireito(new No("E"));
         arvore.getRaiz().getDireito().setDireito(new No("F"));
 
-        System.out.println("preOrdem");
-        arvore.preOrdem((arvore.getRaiz()));
-        System.out.println("emOrdem");
-        arvore.emOrdem((arvore.getRaiz()));
-        System.out.println("posOrdem");
-        arvore.posOrdem((arvore.getRaiz()));
+        System.out.println("preOrdemIte");
+        arvore.preOrdemIte((arvore.getRaiz()));
     }
 }
 
